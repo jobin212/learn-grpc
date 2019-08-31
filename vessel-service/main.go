2851e1397cc22447eea189b9c5970bf527de9fad
jobin <jobin212@gmail.com>
2019-08-31 12:15:53 -0700

    multi stage build working, vessel service added with multi stage build

--- vessel-service/main.go
+++ vessel-service/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	"errors"
+	"fmt"
 
-	pb "github.com/jobin212/shippy-vessel-service/proto/vessel"
+	pb "github.com/jobin212/shippy/vessel-service/proto/vessel"
 	"github.com/micro/go-micro"
 )
 
@@ -29,32 +31,32 @@ func (repo *VesselRepository) FindAvailable(spec *pb.Specification) (*pb.Vessel,
 }
 
 // Our grpc service handler
-type service struc {
-	repo repository
+type service struct {
+	repo Repository
 }
 
-func (s *service) FindAvailable(ctx context.Context, 
-	req *pb.Specification, 
+func (s *service) FindAvailable(ctx context.Context,
+	req *pb.Specification,
 	res *pb.Response) error {
-		// Find the next available vessel
-		vessel, err := s.repo.FindAvailable(req)
-		if err != nil {
-			return err
-		}
-
-		// Set the vessel as part of the response message type
-		res.Vessel = vessel
-		return nil
+	// Find the next available vessel
+	vessel, err := s.repo.FindAvailable(req)
+	if err != nil {
+		return err
 	}
+
+	// Set the vessel as part of the response message type
+	res.Vessel = vessel
+	return nil
+}
 
 func main() {
 	vessels := []*pb.Vessel{
-		&pb.Vessel{Id: "vessel001", Name: "Boaty McBoatface", MaxWeight: 200000, Capacity: 500},		
+		&pb.Vessel{Id: "vessel001", Name: "Boaty McBoatface", MaxWeight: 200000, Capacity: 500},
 	}
 	repo := &VesselRepository{vessels}
 
 	srv := micro.NewService(
-		micro.Name("shippy.service.vessel")
+		micro.Name("shippy.service.vessel"),
 	)
 
 	srv.Init()
