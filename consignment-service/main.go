a62a9d120b0eff569ccf1525faddeb2fc0eb8e05
jobin <jobin212@gmail.com>
2019-08-31 12:42:41 -0700

    Updated json and consignment service

--- consignment-service/main.go
+++ consignment-service/main.go
@@ -5,8 +5,10 @@ import (
 	// Import the generated protobuf code
 	"context"
 	"fmt"
+	"log"
 
 	pb "github.com/jobin212/shippy/consignment-service/proto/consignment"
+	vesselProto "github.com/jobin212/shippy/vessel-service/proto/vessel"
 	"github.com/micro/go-micro"
 )
 
@@ -38,13 +40,29 @@ func (repo *Repository) GetAll() []*pb.Consignment {
 // in the generated code itself for the exact method signatures etc
 // to give you a better idea.
 type service struct {
-	repo repository
+	repo         repository
+	vesselClient vesselProto.VesselServiceClient
 }
 
 // CreateConsignment - we created just one method on our service,
 // which is a create method, which takes a context and a request as an
 // argument, these are handled by the gRPC server.
 func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, res *pb.Response) error {
+
+	// Here we call a client instance of our vessel service with our consignment weight,
+	// and the amount of containers as the capacity value
+	vesselResponse, err := s.vesselClient.FindAvailable(context.Background(), &vesselProto.Specification{
+		MaxWeight: req.Weight,
+		Capacity:  int32(len(req.Containers)),
+	})
+	log.Printf("Foudn vessel: %s \n", vesselResponse.Vessel.Name)
+	if err != nil {
+		return err
+	}
+
+	// We set the VesselId as the vessel we got back from our
+	// vessel service
+	req.VesselId = vesselResponse.Vessel.Id
 
 	// Save our consignment
 	consignment, err := s.repo.Create(req)
@@ -80,8 +98,10 @@ func main() {
 	// Init will parse the command line flags
 	srv.Init()
 
-	// Register handler
-	pb.RegisterShippingServiceHandler(srv.Server(), &service{repo})
+	vesselClient := vesselProto.NewVesselServiceClient("shippy.service.vessel", srv.Client())
+
+	// Register handlers
+	pb.RegisterShippingServiceHandler(srv.Server(), &service{repo, vesselClient})
 
 	if err := srv.Run(); err != nil {
 		fmt.Println(err)
